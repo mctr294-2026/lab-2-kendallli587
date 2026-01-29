75c2e5a89d71f24756298be691aa32fe8e8a4751
Kendall Li <ksli1@ualberta.ca>
2026-01-29 08:06:59 -0700

    Update roots.cpp

--- src/roots/roots.cpp
+++ src/roots/roots.cpp
@@ -1,9 +1,35 @@
 #include "roots.hpp"
 
+// cmake -S . -B build
+// cmake --build build --config Debug
+// .\build\roots_demo.exe
+
 bool bisection(std::function<double(double)> f,
                double a, double b,
                double *root) {
-    return false;
+
+    if (f(a) * f(b) >= 0) {
+        return false; 
+    }
+
+    // n: number of iterations
+    int n = static_cast<int>(std::ceil(log2((b - a) / 1e-6)));
+
+    double temp;
+
+    for (int i = 0; i < n; i++) {
+        temp = (a + b) / 2.0;
+
+        if (f(temp) > 0) {
+            b = temp;
+        } else {
+            a = temp;
+        }
+    }
+
+    *root = temp;
+
+    return true;
 }
 
 bool regula_falsi(std::function<double(double)> f,
