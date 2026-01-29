8c1ce53b1b0451adc5ebf6ed87f2e224e6388f92
Kendall Li <ksli1@ualberta.ca>
2026-01-28 21:38:08 -0700

    Update roots.cpp

--- src/roots/roots.cpp
+++ src/roots/roots.cpp
@@ -1,26 +1,8 @@
 #include "roots.hpp"
-#include <iostream>
-#include <cmath>
 
-bool bisection(std::function<double(double)> f, double a, double b, double *root) {
-    // f is some function
-    // (a,b) are lower and upper bounds
-    
-    // n: number of iterations
-    int n = static_cast<int>(std::ceil(log2((b - a) / 1e-6)));
-
-    float root;
-
-    for (int i = 0; i < n; i++) {
-        float temp = (a + b) / 2;
-
-        if (temp < 0) {
-            temp = (temp + b) / 2;
-        } else {
-            temp = (a + temp) / 2;
-        }
-    }       
-        
+bool bisection(std::function<double(double)> f,
+               double a, double b,
+               double *root) {
     return false;
 }
 
@@ -42,4 +24,3 @@ bool secant(std::function<double(double)> f,
             double *root) {
     return false;
 }
-
